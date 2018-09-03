005fd1bd90dc1c1e6f158d6980904c96f45eec8b
ntsoft <chk1226@hotmail.com>
2018-09-03 16:55:10 +0900

    *

--- Assets/Scripts/WaterRippleByTexture.cs
+++ Assets/Scripts/WaterRippleByTexture.cs
@@ -2,6 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+class WaveData
+{
+    public Vector2 Center  = Vector2.zero;
+    public int NowClip     = 0;
+    public bool DoPlay     = false;
+}
+
 public class WaterRippleByTexture : MonoBehaviour {
 
 	public Texture2D    WaveTexture;
@@ -9,8 +16,8 @@ public class WaterRippleByTexture : MonoBehaviour {
     public float        Amplitude           = 1.5f;
     public Vector2      TitleWidth          = new Vector2(20, 20);
 
-	private readonly int FPS 			    = 30;
-    private readonly float WaitTime         = 1 / 30;
+	static private readonly int FPS 	    = 30;
+    private readonly float WaitTime         = 1 / FPS;
     private readonly int TotalClip 		    = 36;
 	private readonly int ClipWidth		    = 128;
 	private readonly int ClipHeight		    = 128;
@@ -22,11 +29,11 @@ public class WaterRippleByTexture : MonoBehaviour {
     private Mesh sideMesh;
 	private int planeX;
 	private int planeY;
-    private Vector2 playWaveCenter;
-    //private List<Vector2> playWaveCenter;
+    //private Vector2 playWaveCenter;
+    private List<WaveData> playWaveCenter = new List<WaveData>();
     private int step = 0;
 
-	private bool doPlay = false;
+	//private bool doPlay = false;
 
 	// Use this for initialization
 	void Start () {
@@ -39,7 +46,31 @@ public class WaterRippleByTexture : MonoBehaviour {
 
 
 	}
-	
+
+    void initPlayWaveList()
+    {
+        for (int i = 0; i < 5; i++ )
+        {
+            WaveData wave = new WaveData();
+            wave.DoPlay = false;
+            wave.NowClip = 0;
+            playWaveCenter.Add(wave);
+        }
+    }
+
+    WaveData getNotPlayWave()
+    {
+        for(int i = 0; i < playWaveCenter.Count; i++ )
+        {
+            if(playWaveCenter[i].DoPlay == false)
+            {
+                return playWaveCenter[i];
+            }
+        }
+
+        return null;
+    }
+
 	// Update is called once per frame
 	void Update () {
 		if (Input.GetMouseButtonDown (0)) {
@@ -48,6 +79,10 @@ public class WaterRippleByTexture : MonoBehaviour {
 
 		switch (step) {
 		case 0:
+            initPlayWaveList();
+            step++;
+            break;
+        case 1:
             var allMesh = PlaneRoot.GetComponentsInChildren<MeshFilter>();
             topMesh = allMesh[0].mesh;
             sideMesh = allMesh[1].mesh;
@@ -57,12 +92,26 @@ public class WaterRippleByTexture : MonoBehaviour {
 
 			step++;
 			break;
+            
+        case 2:
+            StartCoroutine(playWaveUpdate());
+            step++;
+            break;
 		}
 	}
 
+    IEnumerator playWaveUpdate()
+    {
+        while(true)
+        {
+            playWavePerFrame();
+            yield return new WaitForSeconds(WaitTime);
+
+        }
+    }
 
 
-	IEnumerator PlayWave(Vector2 center)
+	void playWavePerFrame()
 	{
 		var topVerAry = topMesh.vertices;
         var sideVerAry = sideMesh.vertices;
@@ -75,25 +124,52 @@ public class WaterRippleByTexture : MonoBehaviour {
         float rateX = ClipWidth / TitleWidth.x;
         float rateY = ClipHeight / TitleWidth.y;
 
-        for (int clip = 0; clip < TotalClip; clip++) {
-			Vector2 sPos;
-			sPos.x = (clip % MaxWidthClip) * ClipWidth;
-			sPos.y = (clip / MaxWidthClip) * ClipHeight;
+        //reset plane height
+        for (int y = 0; y < planeY; y++)
+        {
+            for (int x = 0; x < planeX; x++)
+            {
+                topVerAry[x + y * planeX].y = 0;
+
+                if(y == 0)
+                {
+                    sideVerAry[x].z = 0;
+                }
+            }
+        }
+        //sideVerAry[planeX].z = 0;
+
+        for (int i = 0; i < playWaveCenter.Count; i++ )
+        {
+            WaveData wave = playWaveCenter[i];
+            if(wave.DoPlay == false)
+            {
+                continue;
+            }
+
+            int clip = wave.NowClip;
+            Vector2 center = wave.Center;
+
+            Vector2 sPos;
+            sPos.x = (clip % MaxWidthClip) * ClipWidth;
+            sPos.y = (clip / MaxWidthClip) * ClipHeight;
 
             var texData = WaveTexture.GetPixels((int)sPos.x, (int)sPos.y, ClipWidth, ClipHeight);
 
             Vector2 pivot = new Vector2(center.x - (TitleWidth.x / 2), center.y - (TitleWidth.y / 2));
-            for (int y = 0; y < TitleWidth.y; y++) {
-				for (int x = 0; x < TitleWidth.x; x++) {
+            for (int y = 0; y < TitleWidth.y; y++)
+            {
+                for (int x = 0; x < TitleWidth.x; x++)
+                {
 
                     int nowX = (int)pivot.x + x;
-                    if(nowX < 0 || nowX > planeX)
+                    if (nowX < 0 || nowX > planeX)
                     {
                         continue;
                     }
 
                     int nowY = (int)pivot.y + y;
-                    if(nowY < 0 || nowY > planeY)
+                    if (nowY < 0 || nowY > planeY)
                     {
                         continue;
                     }
@@ -110,14 +186,14 @@ public class WaterRippleByTexture : MonoBehaviour {
                     //var value = WaveTexture.getp;
                     //verAry [x + y * planeX].y = value;
                     var value = (texData[basePos].r) - OffsetColorValue;
-                    topVerAry[nowX + nowY * planeX].y = value * Amplitude;
+                    topVerAry[nowX + nowY * planeX].y += value * Amplitude;
 
                     //Debug.Log (value);
 
                     // side plane
-                    if(nowY == 0)
+                    if (nowY == 0)
                     {
-                        sideVerAry[nowX + nowY * planeX].z = value * Amplitude * -1;
+                        sideVerAry[planeX - nowX].z += value * Amplitude * -1;
                     }
 
 
@@ -126,29 +202,32 @@ public class WaterRippleByTexture : MonoBehaviour {
             }
 
 
-			topMesh.vertices = topVerAry;
-			topMesh.RecalculateNormals ();
+            wave.NowClip++;
+            if(wave.NowClip == TotalClip)
+            {
+                wave.DoPlay = false;
+            }
+               
+        }
 
-            sideMesh.vertices = sideVerAry;
-            sideMesh.RecalculateNormals();
+        topMesh.vertices = topVerAry;
+        topMesh.RecalculateNormals();
 
-            yield return new WaitForSeconds(WaitTime);
-		}
+        sideMesh.vertices = sideVerAry;
+        sideMesh.RecalculateNormals();
 
-		doPlay = false;
     }
-		
-
 
 	void mouseClick()
 	{
-		if (doPlay == false) {
-			doPlay = true;
-            playWaveCenter.x = planeY / 2;
-            playWaveCenter.y = 0;
+        var wave = getNotPlayWave();
+        if (wave != null)
+        {
+            wave.Center.x = Random.Range(0, planeY);
+            wave.Center.y = 0;
+            wave.DoPlay = true;
+            wave.NowClip = 0;
 
-            StartCoroutine(PlayWave(playWaveCenter));
 		}
-		//playWaveCenter.Add (Vector2 (planeX / 2, planeY / 2));
 	}
 }
